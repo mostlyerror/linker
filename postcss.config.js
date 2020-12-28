815d47863b8e43db7c08a6766b139a0b9426c428
Benjamin Poon <benjaminpoon@nerv.local>
2020-12-28 10:50:54 -0700

    install tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -10,3 +12,4 @@ module.exports = {
     })
   ]
 }
+
