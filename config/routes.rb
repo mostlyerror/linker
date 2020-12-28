2088a60a52cde200277125919ac86b03b6997893
Ben Poon <benjamintpoon@gmail.com>
2020-12-28 10:22:04 -0700

    Basic Authentication (#1)
    * add devise
    
    * authenticated vs public controller(s)
    
    Co-authored-by: Benjamin Poon <benjaminpoon@nerv.local>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    unlocks: 'users/unlocks',
+  }
 end
