4ae93d8eefb663719ad0580725010a09a71c6903
Benjamin Poon <benjamintpoon@gmail.com>
2020-12-28 22:39:54 -0700

    configuring tailwind

--- linker/settings.py
+++ linker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'linker_theme',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+TAILWIND_APP_NAME = 'linker_theme'
 
 ROOT_URLCONF = 'linker.urls'
 
---
+++ linker_theme/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LinkerThemeConfig(AppConfig):
+    name = 'linker_theme'
